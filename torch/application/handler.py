68689be3389604dacf6ad5ade200b7dbaeea227e
Sarath C Akurathi <sarath.raja003@gmail.com>
2020-02-13 16:46:57 +0530

    Updated with latest code

--- torch/application/handler.py
+++ torch/application/handler.py
@@ -1,5 +1,18 @@
 from .actions import Processor
+from .config import helper
+from .config.helper import LIFECYCLE_ACTIONS
 
 def handle_action(action, *args, **kwargs):
     process_handler = Processor()
-    process_handler.process_action(action)
+    context = {}
+    context['lifecycle'] = False
+    context['action'] = action
+    for lc_action in LIFECYCLE_ACTIONS:
+        if context['action'] == lc_action.value:
+            context['lifecycle'] = True
+    
+    if context['lifecycle']:
+        helper.set_context_for_build()
+    
+    process_handler.process_action(context, *args, **kwargs)
+    
